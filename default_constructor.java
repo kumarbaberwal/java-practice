088f3ddd802d47f6a9275c52251233804c00bb80
kumarbaberwal <niteshkumarghn1@gmail.com>
2024-10-05 23:25:01 +0530

    default constructor

---
+++ default_constructor.java
@@ -0,0 +1,10 @@
+public class default_constructor {
+    String name;
+    int age;
+    public static void main(String[] args) {
+        default_constructor obj = new default_constructor();
+
+        System.out.println(obj.name);
+        System.out.println(obj.age);
+    }
+}
