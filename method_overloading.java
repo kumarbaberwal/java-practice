1f3c8acea6593586b8a6737885b4ea78a86fa440
kumarbaberwal <niteshkumarghn1@gmail.com>
2024-10-07 22:18:41 +0530

    method overloading

---
+++ method_overloading.java
@@ -0,0 +1,17 @@
+public class method_overloading{
+    public void add(int a, int b){
+        System.out.println("Sum is" + a+b);
+        // return a + b;
+    }
+    public void add(int a, int b, int c){
+        System.out.println("Sum is" + a+b+c);
+        // return a + b;
+    }
+
+    public static void main(String[] args) {
+        method_overloading obj = new method_overloading();
+
+        obj.add(2, 4);
+        obj.add(4, 7, 9);
+    }
+}
